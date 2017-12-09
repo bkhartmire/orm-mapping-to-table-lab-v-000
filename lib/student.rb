c87da76d475d90815ac331bab6da689816148fac
Brittany Hartmire <bkhartmire@gmail.com>
2017-12-09 10:03:45 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -1,6 +1,15 @@
 class Student
-
-  # Remember, you can access your database connection anywhere in this class
-  #  with DB[:conn]  
+  attr_accessor :name, :grade
+  def initialize(name, grade, id=nil)
+    @name = name
+    @grade = grade
+    @id = id
+  end
+  def self.create_table
+  end
+  def save
+  end
+  def drop_table
+  end 
   
 end
